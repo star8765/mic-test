b21d070845862bf05eb229db9c02a19266dc3815
star8765 <hikari.star8765@gmail.com>
2025-07-31 13:56:33 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+
+const CACHE_NAME = 'mic-visual-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
